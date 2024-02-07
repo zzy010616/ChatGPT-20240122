b8d110c009b9922591e8806d0d02232434fe7cb6
zzy010616 <85182203+zzy010616@users.noreply.github.com>
2024-02-07 11:53:58 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          欢迎使用青鸾阁GPT，建立属于你自己的专属人工智能.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
